268398ef94bce50fdbdc56941a00cd58d34008d2
Carlos Picart <cpicart@siteworx.com>
2016-08-29 11:42:12 -0400

    Updated slush file to remove username dependency

--- slushfile.js
+++ slushfile.js
@@ -15,25 +15,38 @@ var gulp = require('gulp'),
     replace = require('gulp-replace'),
     rename = require('gulp-rename'),
     _ = require('underscore.string'),
-    inquirer = require('inquirer');
+    inquirer = require('inquirer'),
+    path = require('path');
  
 function format(string) {
     var username = string.toLowerCase();
     return username.replace(/\s/g, '');
 }
  
-var defaults = (function() {
-    var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
-        workingDirName = process.cwd().split('/').pop().split('\\').pop(),
-        osUserName = homeDir && homeDir.split('/').pop() || 'root',
-        configFile = homeDir + '/.gitconfig',
-        user = {};
+var defaults = (function () {
+    var workingDirName = path.basename(process.cwd()),
+      homeDir, osUserName, configFile, user;
+
+    if (process.platform === 'win32') {
+        homeDir = process.env.USERPROFILE;
+        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+    }
+    else {
+        homeDir = process.env.HOME || process.env.HOMEPATH;
+        osUserName = homeDir && homeDir.split('/').pop() || 'root';
+    }
+
+    configFile = path.join(homeDir, '.gitconfig');
+    user = {};
+
     if (require('fs').existsSync(configFile)) {
         user = require('iniparser').parseSync(configFile).user;
     }
+
     return {
         appName: workingDirName,
-        userName: format(user.name) || osUserName,
+        userName: osUserName || format(user.name || ''),
+        authorName: user.name || '',
         authorEmail: user.email || ''
     };
 })();
