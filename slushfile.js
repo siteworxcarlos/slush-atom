5bf16685a5ed584bfc1f6a8c8c22d825c59e36e9
Allison Carnwath <acarnwath@siteworx.com>
2016-12-07 17:54:03 -0500

    Added a config file - using local if possible, otherwise falling back to the general one. Separated out the tasks for clarity and easier order control - noted where gulp4.0 will break (need to update to `gulp.series` and `gulp.parallel`) Added prompts for what folder it should go into and what the order # should be for the mustache file generation. Broke out each write to _style.scss into their own stream in case each partial writes to a different file (e.g. _base.scss, _medium.scss) - caused some issues in ordering when all were writing to the same one that were fixed. Added the ability (via the config) to replace comments in the import files (i.e. `//scaffold-desktop` and `//scaffold-mobile`) or simply append to the file. Took out default atom name - made it required.

--- slushfile.js
+++ slushfile.js
@@ -5,108 +5,139 @@
  * Copyright (c) 2016, Carlos
  * Licensed under the MIT license.
  */
-
-'use strict';
- 
 var gulp = require('gulp'),
     install = require('gulp-install'),
     conflict = require('gulp-conflict'),
     template = require('gulp-template'),
     replace = require('gulp-replace'),
+    insert = require('gulp-insert'),
     rename = require('gulp-rename'),
+    gulpif = require('gulp-if'),
     _ = require('underscore.string'),
     inquirer = require('inquirer'),
-    path = require('path');
- 
-function format(string) {
-    var username = string.toLowerCase();
-    return username.replace(/\s/g, '');
+    path = require('path'),
+    config = null,
+    latestAnswers = null;
+
+try {
+    config = require(process.cwd() + '/slush-atom-config.json'); // Try to get local-to-project config
+} catch(e) {
+    config = require('./slush-atom-config.json'); // Fall back to generic config
 }
- 
-var defaults = (function () {
-    var workingDirName = path.basename(process.cwd()),
-      homeDir, osUserName, configFile, user;
 
-    if (process.platform === 'win32') {
-        homeDir = process.env.USERPROFILE;
-        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    }
-    else {
-        homeDir = process.env.HOME || process.env.HOMEPATH;
-        osUserName = homeDir && homeDir.split('/').pop() || 'root';
-    }
-
-    configFile = path.join(homeDir, '.gitconfig');
-    user = {};
-
-    if (require('fs').existsSync(configFile)) {
-        user = require('iniparser').parseSync(configFile).user;
-    }
-
-    return {
-        appName: workingDirName,
-        userName: osUserName || format(user.name || ''),
-        authorName: user.name || '',
-        authorEmail: user.email || ''
-    };
-})();
- 
-gulp.task('default', function (done) {
+gulp.task('refreshAnswers', function(done) {
     var prompts = [{
-        name: 'appName',
-        message: 'What is the name of your atom?',
-        default: defaults.appName
-    }];
+            name: 'appName',
+            message: 'What is the name of your atom?',
+            validate: function(val) {
+                if(val && val.length > 0) { return true; }
+                return 'Please provide a name to use for your atom.';
+            }
+        },{
+            name: 'folder',
+            message: 'What folder should the atom pattern (mustache) be stored in?',
+            default: function(answers) {
+                return '00-'+answers.appName;
+            }
+        },{
+            name: 'orderNumber',
+            message: 'What order should this atom be in, in that folder? (ex. 00 is first)',
+            default: '00'
+        }
+    ];
     //Ask
-    inquirer.prompt(prompts,
-        function (answers) {
-            answers.appNameSlug = _.slugify(answers.appName);
-            //output mustache file in project directory
-            gulp.src(__dirname + '/templates/*.mustache')
-                .pipe(template(answers))
-                .pipe(rename(function(file) {
-                    file.basename = answers.appName;
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('source/_patterns/00-atoms/00-'+answers.appName))
-                .pipe(install())
-                .on('end', function() {
-                    done();
-                });
-
-            //output scss file in project directory
-            gulp.src(__dirname + '/templates/app-sm.scss')
-                .pipe(template(answers))
-                .pipe(rename(function(file) {
-                    file.basename = '_'+answers.appName + "-sm";
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('source/css/scss/modules/'+answers.appName))
-                .pipe(install())
-                .on('end', function() {
-                    done();
-                });
-
-            gulp.src(__dirname + '/templates/app-lg.scss')
-                .pipe(template(answers))
-                .pipe(rename(function(file) {
-                    file.basename = '_'+answers.appName + "-lg";
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('source/css/scss/modules/'+answers.appName))
-                .pipe(install())
-                .on('end', function() {
-                    done();
-                }); 
-            
-            //update scss file
-            gulp.src('source/css/*.scss')
-                .pipe(replace('//scaffold-mobile','@import "scss/modules/'+answers.appName+'/'+answers.appName+'-sm";\n//scaffold-mobile'))
-                .pipe(replace('//scaffold-desktop','@import "scss/modules/'+answers.appName+'/'+answers.appName+'-lg";\n//scaffold-desktop'))
-                .pipe(gulp.dest('source/css/'))
-                .pipe(install())
-                .on('end', function() {
-                    done();
-                });
-        });
+    inquirer.prompt(prompts, function (answers) {
+        latestAnswers = answers;
+        gulp.start('createFiles'); // NOTE: Will be deprecated in gulp 4.0
+        done();
+    });
 });
+
+function haveAnswers() {
+    if(!latestAnswers) {
+        console.error('Need to run task `refreshAnswers` before creating files');
+        return false;
+    }
+
+    return true;
+}
+
+gulp.task('createPattern', function(done) {
+    if(!haveAnswers()) { return; }
+
+    //output mustache file in project directory
+    return gulp.src(__dirname + '/templates/*.mustache')
+        .pipe(template(latestAnswers))
+        .pipe(rename(function(file) {
+            file.basename = latestAnswers.orderNumber + '-' + latestAnswers.appName;
+        }))
+        .pipe(conflict('./'))
+        .pipe(gulp.dest(config.patternOutputPath + latestAnswers.folder))
+        .pipe(install());
+});
+
+var scssFileTasks = [],
+    lastScssImportTask = null;
+(function () {
+    for(var i = 0; i < config.scssFiles.length; i++) {
+        var fileInfo = config.scssFiles[i],
+            taskName = 'createScssFile-' + i;
+
+        createScssFileTask(fileInfo, taskName);
+        scssFileTasks.push(taskName);
+
+        // Since these all are potentially writing to the same file, make each one dependent on the last one completing
+        // only need to track the name of the last one to trigger the whole chain
+        taskName = 'importScssFile-' + i;
+        createScssImportTask(fileInfo, taskName, (i === 0 ? null : ['importScssFile-' + (i - 1)]));
+        lastScssImportTask = taskName;
+    }
+})();
+function createScssFileTask(fileInfo, taskName) {
+    gulp.task(taskName, function(done) {
+        if(!haveAnswers()) { return; }
+
+        var src = fileInfo.slushTemplate ? __dirname + fileInfo.template : fileInfo.template,
+            filename = latestAnswers.appName + config.scssNamespaceConnector + fileInfo.name;
+
+        // Output folder with scss files in project directory
+        return gulp.src(src)
+            .pipe(template(latestAnswers))
+            .pipe(rename(function(file) {
+                file.basename = "_" + filename;
+            }))
+            .pipe(conflict(config.scssFileOutputPath + latestAnswers.appName))
+            .pipe(gulp.dest(config.scssFileOutputPath + latestAnswers.appName))
+            .pipe(install());
+    });
+}
+
+function createScssImportTask(fileInfo, taskName, prevTask) {
+
+    // NOTE: Will be deprecated in gulp 4.0 - use series instead
+    if(prevTask !== null) { gulp.task(taskName, prevTask, task); }
+    else { gulp.task(taskName, task); }
+
+    function task(done) {
+        if(!haveAnswers()) { return; }
+
+        var filename = latestAnswers.appName + config.scssNamespaceConnector + fileInfo.name,
+            dirName = fileInfo.includedFromFile.substring(0, fileInfo.includedFromFile.lastIndexOf("/")),
+            replacing = (fileInfo.importReplace ? true : false),
+            importStr = '@import "' + fileInfo.scssImportPath + latestAnswers.appName + '/' + filename + '";';
+
+        // Update the scss file that includes this new file
+        return gulp.src(fileInfo.includedFromFile)
+            // Prepend the placeholder text with the new import
+            .pipe(gulpif(replacing, replace(fileInfo.importReplace,importStr+'\n'+fileInfo.importReplace)))
+            // Appending the new import
+            .pipe(gulpif(!replacing, insert.append('\n'+importStr)))
+
+            .pipe(gulp.dest(dirName))
+            .pipe(install());
+    }
+}
+
+// NOTE: Will be deprecated in gulp 4.0, use series and/or parallel instead
+gulp.task('createFiles',['createPattern', lastScssImportTask].concat(scssFileTasks));
+gulp.task('default', ['refreshAnswers']);
